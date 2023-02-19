f9cd065a6804eb7eb268d0c174a3521726d0d6f1
Gubera24 <114172003+Gubera24@users.noreply.github.com>
2023-02-19 12:41:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "9187528"))
+API_HASH = environ.get("API_HASH", "26e7cef4bad59d61a9cca5e600bce2a2")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5734105042:AAGy1WDpSMJ2jbLOAlxm-88SNWDW8OEqsM4")
+ADMIN = int(environ.get("ADMIN", "2087926333"))          
+CAPTION = environ.get("CAPTION", "{file_name}
+
+💽 Size = {file_size}
+
+📯Join My Channel : @Tamilan24")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
